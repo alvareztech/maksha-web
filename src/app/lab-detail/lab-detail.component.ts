f2f0d8f93afe0755b741571265a25ef97a161e10
Daniel Alvarez <a3dany@gmail.com>
2017-04-15 15:53:15 -0400

    Lab banner title and icon

--- src/app/lab-detail/lab-detail.component.ts
+++ src/app/lab-detail/lab-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 import {ActivatedRoute, Params} from '@angular/router';
 import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
+import {TechnologyService} from '../services/technology.service';
 
 @Component({
   selector: 'app-lab-detail',
@@ -16,7 +17,7 @@ export class LabDetailComponent implements OnInit {
   currentStep: number;
   labObject: object;
 
-  constructor(private route: ActivatedRoute, private af: AngularFire) {
+  constructor(private route: ActivatedRoute, private af: AngularFire, private technologyService: TechnologyService) {
   }
 
   ngOnInit() {
@@ -33,6 +34,7 @@ export class LabDetailComponent implements OnInit {
       });
     });
   }
+
   changeStep(i: number, title: string, content: string) {
     console.log('step selected: ' + i);
     this.currentStep = i;
