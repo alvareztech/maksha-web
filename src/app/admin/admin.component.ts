2f916c1005e045ad56150e8d38a3143833abaf62
Daniel Alvarez <a3dany@gmail.com>
2017-05-08 14:59:27 -0400

    Add articles admin module

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -10,7 +10,6 @@ import {MdSnackBar} from '@angular/material';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-  successMessage = '';
   currentLab = {
     $key: '',
     title: '',
@@ -24,54 +23,63 @@ export class AdminComponent implements OnInit {
     content: ''
   };
   isNewLab = true; // update otherwise
+  isNewArticle = true; // update otherwise
+
+  currentArticle = {};
 
   lab: FirebaseObjectObservable<any>;
   labs: FirebaseListObservable<any>;
+  article: FirebaseObjectObservable<any>;
+  articles: FirebaseListObservable<any>;
 
   currentStepNumber = 0;
 
   isLabSelected = false;
+  isArticleSelected = false;
 
   constructor(public technologyService: TechnologyService, private db: AngularFireDatabase, public snackBar: MdSnackBar) {
     this.labs = db.list('/labs');
+    this.articles = db.list('/articles');
   }
 
   ngOnInit() {
   }
 
+  // Labs methods
+
   saveLab() {
     this.currentLab.steps[this.currentStepNumber] = this.currentStep;
-    console.log('saveLab(): %j', this.currentLab);
     this.currentLab['updated'] = firebase.database.ServerValue.TIMESTAMP;
-    // this.successMessage = 'Se guardo correctamente';
-    if (this.isNewLab) {
-      this.labs.update(this.currentLab['$key'], {
-        title: this.currentLab['title'],
-        updated: firebase.database.ServerValue.TIMESTAMP,
-        technology: this.currentLab['technology'],
-        level: +this.currentLab['level'],
-        published: false,
-        steps: [{
-          title: 'Resumen',
-          content: 'Resumen \n\n## Aprenderás\n\n* Primero\n\n## Requisitos\n\n* Otro'
-        }]
-      }).then(a => {
-        this.snackBar.open('New lab successfully saved!', null, { duration: 2000 });
-      });
-    } else {
-      this.currentLab['level'] = +this.currentLab['level'];
-      this.lab.update(this.currentLab).then(a => {
-        this.snackBar.open('Successfully saved!', null, { duration: 2000 });
-      });
-    }
+
+    this.labs.update(this.currentLab['$key'], {
+      title: this.currentLab['title'],
+      updated: firebase.database.ServerValue.TIMESTAMP,
+      technology: this.currentLab['technology'],
+      level: +this.currentLab['level'],
+      published: false,
+      steps: [{
+        title: 'Resumen',
+        content: 'Resumen \n\n## Aprenderás\n\n* Primero\n\n## Requisitos\n\n* Otro'
+      }]
+    }).then(a => {
+      this.snackBar.open('New lab successfully saved!', null, {duration: 2000});
+    });
+  }
+
+  updateLab() {
+    this.currentLab.steps[this.currentStepNumber] = this.currentStep;
+    this.currentLab['updated'] = firebase.database.ServerValue.TIMESTAMP;
+
+    this.currentLab['level'] = +this.currentLab['level'];
+    this.lab.update(this.currentLab).then(a => {
+      this.snackBar.open('Lab successfully updated!', null, {duration: 2000});
+    });
   }
 
   goLab(o: any) {
-    console.log('Id ' + o.$key + ' pressed');
-
     this.isLabSelected = true;
-
     this.isNewLab = false;
+
     this.lab = this.db.object('/labs/' + o.$key);
 
     this.lab.forEach(value => {
@@ -117,6 +125,61 @@ export class AdminComponent implements OnInit {
 
   returnLabs() {
     this.isLabSelected = false;
+  }
+
+  // Articles methods
+
+  saveArticle() {
+    this.articles.update(this.currentArticle['$key'], {
+      title: this.currentArticle['title'],
+      content: this.currentArticle['content'],
+      updated: firebase.database.ServerValue.TIMESTAMP,
+      created: firebase.database.ServerValue.TIMESTAMP,
+    }).then(a => {
+      this.snackBar.open('New article successfully saved!', null, {duration: 2000});
+    }).catch(a => {
+      this.snackBar.open('Error:' + a.message, null, {duration: 2000});
+    });
+  }
+
+  updateArticle() {
+    this.articles.update(this.currentArticle['$key'], {
+      title: this.currentArticle['title'],
+      content: this.currentArticle['content'],
+      published: this.currentArticle['published'],
+      updated: firebase.database.ServerValue.TIMESTAMP
+    }).then(a => {
+      this.snackBar.open('Article successfully updated!', null, {duration: 2000});
+    }).catch(a => {
+      this.snackBar.open('Error:' + a.message, null, {duration: 2000});
+    });
+  }
+
+  goArticle(art: any) {
+    this.isArticleSelected = true;
+    this.isNewArticle = false;
+
+    this.article = this.db.object('/articles/' + art.$key);
+    this.article.subscribe(a => {
+      console.log('Llego art: ', a);
+      this.currentArticle = a;
+    });
+  }
+
+  goNewArticle() {
+    this.isArticleSelected = true;
+    this.isNewArticle = true;
+
+    this.currentArticle = {
+      $key: '',
+      title: 'New article',
+      published: false,
+      content: 'Any content'
+    };
+  }
+
+  returnArticles() {
+    this.isArticleSelected = false;
   }
 
 }
