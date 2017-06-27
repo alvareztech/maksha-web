e012dacc755221da102fa2793e873c7a711ee226
Daniel Alvarez <a3dany@gmail.com>
2017-06-27 17:50:57 -0400

    Add moderator comments, Firebase Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,11 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+const Filter = require('bad-words');
+const badWordsFilter = new Filter({ list: ['mierda', 'carajo'], placeHolder: '😮' });
+
+const capitalizeSentence = require('capitalize-sentence');
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -64,3 +69,59 @@ exports.saveUserInformation = functions.auth.user().onCreate(event => {
     photoURL: photoURL
   });
 });
+
+exports.moderator = functions.database.ref('/comments/{placeComment}/{itemId}/{commentId}').onWrite(event => {
+  const message = event.data.val();
+
+  if (message && !message.sanitized) {
+    console.log('Retrieved message content: ', message);
+
+    const moderatedMessage = moderateMessage(message.content);
+
+    console.log('Message has been moderated. Saving to DB: ', moderatedMessage);
+    return event.data.adminRef.update({
+      content: moderatedMessage,
+      sanitized: true,
+      moderated: message.content !== moderatedMessage
+    });
+  }
+});
+
+// Moderates the given message if appropriate.
+function moderateMessage(message) {
+  // Re-capitalize if the user is Shouting.
+  if (isShouting(message)) {
+    console.log('User is shouting. Fixing sentence case...');
+    message = stopShouting(message);
+  }
+
+  // Moderate if the user uses SwearWords.
+  if (containsSwearwords(message)) {
+    console.log('User is swearing. moderating...');
+    message = moderateSwearwords(message);
+  }
+
+  return message;
+}
+
+// Returns true if the string contains swearwords.
+function containsSwearwords(message) {
+  return message !== badWordsFilter.clean(message);
+}
+
+// Hide all swearwords. e.g: Crap => ****.
+function moderateSwearwords(message) {
+  return badWordsFilter.clean(message);
+}
+
+// Detect if the current message is shouting. i.e. there are too many Uppercase
+// characters or exclamation points.
+function isShouting(message) {
+  return message.replace(/[^A-Z]/g, '').length > message.length / 2 || message.replace(/[^!]/g, '').length >= 3;
+}
+
+// Correctly capitalize the string as a sentence (e.g. uppercase after dots)
+// and remove exclamation points.
+function stopShouting(message) {
+  return capitalizeSentence(message.toLowerCase()).replace(/!+/g, '.');
+}
